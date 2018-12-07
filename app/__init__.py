06021f2599b4a98e68e101be204ed77b58467d1e
root <root@kubetest.byu.edu>
2018-12-07 15:20:22 +0000

    add website stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, Response
 import uuid
 app = Flask(__name__)
-
-
 @app.route("/")
 def hello():
-    return Response(str(uuid.uuid4()), status=200, mimetype='text/plain')
+    r = open("../message.txt","r")
+    f = r.readline()
+    return Response(f.strip(), status=200, mimetype='text/plain')
